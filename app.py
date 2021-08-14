f6a2bd518520ac4346146743554c4f590c0d4303
Akash S <akashsureshmyd@gmail.com>
2021-08-14 22:16:07 +0530

    updated index.html connection

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,url_for
+from flask.templating import render_template
 
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
-    return "hi"
+    return render_template('index.html')
 
 
 app.run(debug=True)
